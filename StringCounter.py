95f138eb594dcb91844b4de4470740c440edb75c
notChewy1324 <40218645+notChewy1324@users.noreply.github.com>
2019-04-24 20:42:24 -0400

    Add files via upload

---
+++ StringCounter.py
@@ -0,0 +1,6 @@
+#Create a program called string counter
+#Take users input and return the number of characters in the string
+letters = input("")
+type(letters)
+#Display Number
+print(len(letters))
