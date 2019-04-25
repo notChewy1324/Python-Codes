dbfddc9b31eee74601bfb88e9045d09828b63e66
notChewy1324 <40218645+notChewy1324@users.noreply.github.com>
2019-04-24 20:53:05 -0400

    Add files via upload

---
+++ PasswordCracker.py
@@ -0,0 +1,15 @@
+#Create a program called password cracker 
+#Create the secret code
+secret = "1234"
+#Create a password
+pword = "123"
+#Display the output if input is the secret code or pword
+print("Enter the Password!")
+x = input("")
+type(x)
+if (x == secret) :
+	print("You just hacked 'EM!")
+elif (x == pword) :
+	print("You just got HACKED!")
+else : 
+	print("error")
