ecf70eb9e3919bcece98086bbcdbec2a1565a2d9
notChewy1324 <40218645+notChewy1324@users.noreply.github.com>
2019-04-26 17:24:12 -0400

    Add files via upload

---
+++ FavoriteMovie.py
@@ -0,0 +1,7 @@
+#Create program called fav moive
+#Take users input and save it locally
+print("What is your favorite movie?")
+user = input("")
+type(user)
+#Display users input
+print("Your favorite is",user)
