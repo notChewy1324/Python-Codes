eca0be2198695c525edfe0a0acc4cf0377a94f96
notChewy1324 <40218645+notChewy1324@users.noreply.github.com>
2019-05-11 22:37:41 -0400

    Add files via upload

---
+++ DoggySurvey.py
@@ -0,0 +1,28 @@
+#Create a program called Doggy Survey
+#import time
+import time
+#Ask the user a question
+#Take there input from a scale of one to ten
+#Ask 4 questions
+print("Welcome to the Doggy Survey")
+time.sleep(3)
+print("It's about to begin")
+time.sleep(2)
+print("From a scale 1-10. Are dogs fun to play with?")
+user0 = input("")
+print("From a scale 1-10. Are dogs better than cats?")
+user1 = input("")
+print("From a scale 1-10. Are dogs boring?")
+user2 = input("")
+print("From a scale 1-10. Did you like this survey?")
+user3 = input("")
+#Display user input
+print("You rated",user0,"for question 1")
+time.sleep(2)
+print("You rated",user1,"for question 2")
+time.sleep(2)
+print("You rated",user2,"for question 3")
+time.sleep(2)
+print("You rated",user3,"for question 4")
+time.sleep(2)
+print("And hopefully you enjoyed this doggy survey")
---
+++ SentenceMaker.py
@@ -0,0 +1,9 @@
+#Create program called sentence maker
+#Take user input and save it locally
+print("Give me a noun")
+user = input("")
+#Take user input again and save it locally
+print("Give me a verb")
+userverb = input("")
+#Display user input
+print("There was a",user,"that",userverb)
