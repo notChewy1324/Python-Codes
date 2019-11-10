fc587d4317451e91c1432c1a50fd212b1093317b
NotChewy1324 <camerongarrison4@gmail.com>
2019-11-10 15:16:06 -0500

    Create MadLibGenerator.py

---
+++ MadLibGenerator.py
@@ -0,0 +1,33 @@
+import random
+libnum = random.randint(1,5)
+libnum = int(libnum)
+if (libnum == 1):
+    userNoun = input("Give me a noun: ")
+    userNoun = str(userNoun)
+    userVerb = input("Give me a verb: ")
+    userVerb = str(userVerb)
+    print("My name is",userNoun,"I love to",userVerb,"!")
+if (libnum == 2):
+    userNoun = input("Give me a noun: ")
+    userNoun = str(userNoun)
+    userVerb = input("Give me a verb: ")
+    userVerb = str(userVerb)
+    print("Why is",userNoun,"really mean to",userVerb,"!")
+if (libnum == 3):
+    userNoun = input("Give me a noun: ")
+    userNoun = str(userNoun)
+    userVerb = input("Give me a verb: ")
+    userVerb = str(userVerb)
+    print(userNoun,"needs to stop what they always do!",userVerb,"is what I want to do now!")
+if (libnum == 4):
+    userNoun = input("Give me a noun: ")
+    userNoun = str(userNoun)
+    userVerb = input("Give me a verb: ")
+    userVerb = str(userVerb)
+    print("My hobby is",userNoun,"I want to",userVerb,"!")
+if (libnum == 5):
+    userNoun = input("Give me a noun: ")
+    userNoun = str(userNoun)
+    userVerb = input("Give me a verb: ")
+    userVerb = str(userVerb)
+    print("What are",userNoun,"and why do they",userVerb,"!")
