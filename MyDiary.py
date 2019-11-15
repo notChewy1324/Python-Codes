7960544299250070cda7e41e206184ffa646b194
NotChewy1324 <camerongarrison4@gmail.com>
2019-11-14 19:05:38 -0500

    Newer Version
    This has new comments for future plans and better input code and questions.

--- MyDiary.py
+++ MyDiary.py
@@ -2,14 +2,15 @@
 #This is still being worked on.
 import time
 daycount = 1
-                      #ERROR FIX
-user = input("Day " + daycount + " Log:")
+#Need to put into a while(True) loop 
+            #Need to change string to increment by 1
+user = input("Day 1 Log:")
 user = str(user)
 time.sleep(2)
 print("Saved!")
+user1 = input("Would you like to see the past logs? (y or n)")
+if (user1 == "y"):
+    print(user)
 time.sleep(1)
 daycount += 1
-                      #ERORR FIX
-user = input("Day " + daycount + " Log:")
-user = str(user)
 #The current goal is to have a diary that logs information when you save data to it.
