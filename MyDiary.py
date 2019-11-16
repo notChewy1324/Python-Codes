a2d77422ca9f13903f057ff11803d5804b71afdc
NotChewy1324 <camerongarrison4@gmail.com>
2019-11-15 21:57:45 -0500

    Updated Version

--- MyDiary.py
+++ MyDiary.py
@@ -2,15 +2,18 @@
 #This is still being worked on.
 import time
 daycount = 1
-#Need to put into a while(True) loop 
-            #Need to change string to increment by 1
-user = input("Day 1 Log:")
-user = str(user)
-time.sleep(2)
-print("Saved!")
-user1 = input("Would you like to see the past logs? (y or n)")
-if (user1 == "y"):
-    print(user)
-time.sleep(1)
-daycount += 1
+while(True):
+    user = input("Day " + str(daycount) + " Log: ")
+    user = str(user)
+    time.sleep(1)
+    print("Saved!")
+    user1 = input("Would you like to see the past logs or write for the next day? (y or n or next)")
+    if (user1 == "y"):
+        print(user)
+    if (user1 == "next"):
+        time.sleep(1)
+        daycount += 1
+        continue
+    if (user1 != "y" or "next"):
+        break
 #The current goal is to have a diary that logs information when you save data to it.
