afc2812fb3eaef99a1362c7abca028516982e9f7
NotChewy1324 <camerongarrison4@gmail.com>
2019-11-30 20:49:12 -0500

    Create GrowingNumber_v2.py

---
+++ GrowingNumber_v2.py
@@ -0,0 +1,9 @@
+user = input("What number do you want to grow by?: ")
+user = int(user)
+user2 = input("What number do you want to start by?: ")
+user2 = int(user2)
+print(user)
+while(True):
+    user2 = user2 + user
+    print(user2)
+    continue
