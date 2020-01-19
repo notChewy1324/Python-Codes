16205cfbd4f1bb4ce2aac0959a74f14a18332722
notChewy1324 <40218645+notChewy1324@users.noreply.github.com>
2020-01-19 16:24:26 -0500

    Updated Version
    Fixed how to save system works

--- MyDiary.py
+++ MyDiary.py
@@ -1,18 +1,15 @@
-#I am working on saving and taking user data to save locally to there device to resemble a diary.
-#This is still being worked on.
 import time
-daycount = 1
+daycount = 0
 while(True):
     user = input("Day " + str(daycount) + " Log: ")
-    user = str(user)
+    user = str("Day " + str(daycount) + " Log: " + user)
+    f = open('Diary_Logs', "a+")
+    f.write(user + "\n")
+    f.close()
     print("Saved!")
-    time.sleep(1)
-    user1 = input("Would you like to see the past logs or write for the next day? (yes or no or next)")
-    if (user1 == "yes"):
-        print(user)
+    user1 = input("Would you like to write for the next day? (next or quit)")
     if (user1 == "next"):
-        time.sleep(1)
         daycount += 1
         continue
-    if (user1 != "yes" or "next"):
+    if (user1 != "yes"):
         break
