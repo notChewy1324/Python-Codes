5ab5ff2576e96f34c9673932aadbbe8990ddf2cc
notChewy1324 <40218645+notChewy1324@users.noreply.github.com>
2020-04-26 14:49:01 -0400

    Updated Version

--- GrowingNumber_v2.py
+++ GrowingNumber_v2.py
@@ -4,6 +4,6 @@ user2 = input("What number do you want to start by?: ")
 user2 = int(user2)
 print(user)
 while(True):
-    user2 = user2 + user
+    user2 = user2 * user
     print(user2)
     continue
