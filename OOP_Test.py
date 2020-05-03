4f27efc903a67ca71df2ccf6614fa895d6aba76f
notChewy1324 <40218645+notChewy1324@users.noreply.github.com>
2020-05-03 17:55:22 -0400

    Updated Logic

--- OOP_Test.py
+++ OOP_Test.py
@@ -1,3 +1,5 @@
+# OOP in Python
+
 class dog():
 
     def __init__(self, name, age, hrs):
@@ -14,8 +16,20 @@ class dog():
     def dog_toyTime(self):
         return self.hrs
 
+dog1 = input("Whats your first dogs name?: ")
+dog2 = input("Whats your second dogs name?: ")
 
-first = dog("Jackson", 3, 45)
-second = dog("Gracie", 3, 5)
+dog1Age = input("Whats your first dogs age?: ")
+dog1Age = int(dog1Age)
+dog2Age = input("Whats your second dogs age?: ")
+dog2Age = int(dog1Age)
 
-print(first.dog_name(), second.dog_name())
+dog1hrs = input("Whats your first dogs play time?: ")
+dog1hrs = int(dog1hrs)
+dog2hrs = input("Whats your second dogs play time?: ")
+dog2hrs = int(dog2hrs)
+
+firstDog = dog(dog1, dog1Age, dog1hrs)
+secondDog = dog(dog2, dog2Age, dog2hrs)
+
+print(firstDog.dog_name, secondDog.dog_name())
