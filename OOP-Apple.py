bd1cc0fe43f90809a669cfcb4672691eaa9bd998
Cameron Garrison <40218645+notChewy1324@users.noreply.github.com>
2020-07-23 18:09:53 -0400

    Created OOP-Apple.py

---
+++ OOP-Apple.py
@@ -0,0 +1,11 @@
+class Apple():
+    def __init__(self, c, w, s, m):
+        self.color = c
+        self.weight = w # In Ounces
+        self.size = s # In Inches
+        self.mold = m # 0 for no mold & 1 for mold
+    
+
+apple1 = Apple("red", 12, 15, 0)
+apple2 = Apple("blue", 3, 5, 1)
+print(apple2.color)
