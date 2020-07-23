e4707800a71c3b1eed72c22fcb8f10cd90468ec7
Cameron Garrison <40218645+notChewy1324@users.noreply.github.com>
2020-07-23 19:36:42 -0400

    Create FUNC-Increments.py

---
+++ FUNC-Increments.py
@@ -0,0 +1,13 @@
+# Functional Programming
+
+def increment(a):
+    sum = a + 1
+    return sum
+
+def decrement(a):
+    sum = a - 1
+    return sum
+
+# Show Increment
+display = increment(5) # Pass a whole number to be incremented
+print(display)
