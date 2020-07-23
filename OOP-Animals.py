54a05f312e674beb85f750435a1c2fb58a084026
Cameron Garrison <40218645+notChewy1324@users.noreply.github.com>
2020-07-23 19:55:34 -0400

    Create OOP-Animals.py

---
+++ OOP-Animals.py
@@ -0,0 +1,15 @@
+class Dogs():
+    def __init__(self, b, s, c):
+        self.Breed = b
+        self.Size = s
+        self.Color = c
+
+    def SleepTime(self, t):
+        self.sleepTime = t
+    
+    def Bark(self):
+        print("BARK!")
+    
+    def Eat(self):
+        print("EATING")
+
