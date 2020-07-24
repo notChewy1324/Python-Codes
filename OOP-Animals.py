198e5edd3dfc69583c065cfd919e7258cc517c36
Cameron Garrison <40218645+notChewy1324@users.noreply.github.com>
2020-07-23 20:07:55 -0400

    Update OOP-Animals.py

--- OOP-Animals.py
+++ OOP-Animals.py
@@ -1,11 +1,12 @@
 class Dogs():
-    def __init__(self, b, s, c):
+    def __init__(self, b, s, c, w):
         self.Breed = b
         self.Size = s
         self.Color = c
+        self.Weight = w # In pounds
 
     def SleepTime(self, t):
-        self.sleepTime = t
+        self.sleepTime = t # In hrs
     
     def Bark(self):
         print("BARK!")
@@ -13,3 +14,11 @@ class Dogs():
     def Eat(self):
         print("EATING")
 
+# Dogs
+Jackson = Dogs("Poodle", "small", "white", 15)
+Jackson.SleepTime(8)
+
+Gracie = Dogs("Shih Tzu", "medium", "black", 12)
+
+# Display the dogs information
+print(Jackson.Size)
