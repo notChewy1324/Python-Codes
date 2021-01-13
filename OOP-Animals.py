ef25bdf017316a84aaa2b1c1f78504ae4e6cb1bb
Cameron Garrison <40218645+notChewy1324@users.noreply.github.com>
2021-01-13 16:43:15 -0500

    Update OOP-Animals.py

--- OOP-Animals.py
+++ OOP-Animals.py
@@ -6,7 +6,7 @@ class Dogs():
         self.Weight = w # In pounds
 
     def SleepTime(self, t):
-        self.sleepTime = t # In hrs
+        self.SleepTime = t # In hrs
     
     def Bark(self):
         print("BARK!")
