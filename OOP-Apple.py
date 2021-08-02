06af49c5a60953a78c2ac9bf864a39deb33a9861
Cameron Garrison <40218645+notChewy1324@users.noreply.github.com>
2021-08-01 21:38:49 -0400

    added cost def

--- OOP-Apple.py
+++ OOP-Apple.py
@@ -4,9 +4,15 @@ class Apple():
         self.weight = w # In Ounces
         self.size = s # In Inches
         self.mold = m # 0 for no mold & 1 for mold
+
+    def cost(self, s):
+        if s >= 10:
+            return 100
+        else:
+            return 5
     
 
 # Change methods for different outputs
 apple1 = Apple("red", 12, 15, 0)
 apple2 = Apple("blue", 3, 5, 1)
-print(apple2.color)
+print(apple2.cost(1))
