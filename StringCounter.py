dbbf76a423e94eb6b93f5ef8df6fe9f5156b535d
Cameron Garrison <40218645+notChewy1324@users.noreply.github.com>
2021-08-03 23:38:21 -0400

    minor change

--- StringCounter.py
+++ StringCounter.py
@@ -1,6 +1,5 @@
 #Create a program called string counter
 #Take users input and return the number of characters in the string
 letters = input("")
-type(letters)
 #Display Number
 print(len(letters))
