94ec4dafeb3c0290eaf9f9fba1c82b0a9830d459
Cameron Garrison <40218645+notChewy1324@users.noreply.github.com>
2021-08-05 21:48:18 -0400

    changed y/n to 1/0

--- crashAsystem.py
+++ crashAsystem.py
@@ -2,15 +2,15 @@
 #import time
 import time
 #Ask user if they want to crash a System
-user = input("Would you like to crash a system. (y/n): ")
-user = str(user)
+user = input("Would you like to crash a system. (1/0): ")
+user = int(user)
 #Create if statement depending on users input
-if (user == "y") :
+if (user == 1) :
 	print("System is crashing!")
 	time.sleep(5)
 	print("System crashed")
 	time.sleep(1)
 	print("Now Run. The government is after you!!!")
 	
-if (user == "n") :
+if (user == 0) :
 	print("Good Choice.")
